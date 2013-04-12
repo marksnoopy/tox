a44c70b9ef4618650600984268dbf941fb67f900
Snakevil Zen <zsnakevil@gmail.com>
2013-04-12 16:29:44 +0800

    Implemented the prepare type of statements for extended PHP data objects.

---
+++ src/data/pdo/prepare.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Defines the prepare type of statements for extended PHP data objects.
+ *
+ * This file is part of Tox.
+ *
+ * Tox is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Tox is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tox.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @copyright © 2012-2013 PHP-Tox.org
+ * @license   GNU General Public License, version 3
+ */
+
+namespace Tox\Data\Pdo;
+
+use Tox\Data;
+
+/**
+ * Represents as a prepare type statement for an extended PHP data object.
+ *
+ * **THIS CLASS CANNOT BE INSTANTIATED.**
+ *
+ * @package tox.data.pdo
+ * @author  Snakevil Zen <zsnakevil@gmail.com>
+ * @since   0.1.0-beta1
+ */
+class Prepare extends Statement
+{
+    /**
+     * {@inheritdoc}
+     *
+     * @param Data\IPdo $pdo  Hosting data object.
+     * @param string    $sql  Statement SQL.
+     */
+    public function __construct(Data\IPdo $pdo, $sql)
+    {
+        parent::__construct($pdo, $sql);
+        $this->type = self::TYPE_PREPARE;
+    }
+}
+
+// vi:ft=php fenc=utf-8 ff=unix ts=4 sts=4 et sw=4 fen fdm=indent fdl=1 tw=120
